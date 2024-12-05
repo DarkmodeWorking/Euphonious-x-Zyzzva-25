46e9ee06384178be39a76efd859228f35bf95035
Nanda Lal Das <tikludas01@gmail.com>
2024-12-05 17:27:36 +0530

    changed files everything

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com','assets.aceternity.com','img.freepik.com', 'ideogram.ai'],
-      },
-    
-};
-
-export default nextConfig;
