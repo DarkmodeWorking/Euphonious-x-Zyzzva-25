11fa681b98cd70c1b2a6ff8043c65199af97f0cf
Nanda Lal Das <tikludas01@gmail.com>
2024-12-11 18:16:21 +0530

    made iamges to .webp

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -12,94 +12,94 @@ export const products = [
     title: "",
     // link: "https://gomoonbeam.com",
     thumbnail:
-      "shared/DSC_0080.JPG",
+      "shared/DSC_0080.webp",
   },
   {
     title: "",
     // link: "https://cursor.so",
     thumbnail:
-      "shared/DSC06699.JPG",
+      "shared/DSC06699.webp",
   },
   {
     title: "",
     // link: "https://userogue.com",
     thumbnail:
-      "shared/DSC06711.JPG",
+      "shared/DSC06711.webp",
   },
 
   {
     title: "",
     // link: "https://editorially.org",
     thumbnail:
-      "shared/DSC06734.JPG",
+      "shared/DSC06734.webp",
   },
   {
     title: "",
     // link: "https://editrix.ai",
     thumbnail:
-      "shared/DSC06844.JPG",
+      "shared/DSC06844.webp",
   },
   {
     title: "",
     // link: "https://app.pixelperfect.quest",
     thumbnail:
-      "shared/DSC_0115.JPG",
+      "shared/DSC_0115.webp",
   },
 
   {
     title: "",
     // link: "https://algochurn.com",
     thumbnail:
-      "shared/DSC_0140.JPG",
+      "shared/DSC_0140.webp",
   },
   {
     title: "",
     // link: "https://ui.aceternity.com",
     thumbnail:
-      "shared/_MG_0130.jpg",
+      "shared/_MG_0130.webp",
   },
   {
     title: "",
     // link: "https://tailwindmasterkit.com",
     thumbnail:
-      "shared/DSC_9364.JPG",
+      "shared/DSC_9364.webp",
   },
   {
     title: "",
     // link: "https://smartbridgetech.com",
     thumbnail:
-      "shared/DSC_2439.jpg",
+      "shared/DSC_2439.webp",
   },
   // {
   //   title: "",
   //   // link: "https://renderwork.studio",
   //   thumbnail:
-  //     "shared/DSC06708.JPG",
+  //     "shared/DSC06708.webp",
   // },
 
   // {
   //   title: "",
   //   // link: "https://cremedigital.com",
   //   thumbnail:
-  //     "shared/DSC_2502.JPG",
+  //     "shared/DSC_2502.webp",
   // },
   // {
   //   title: "",
   //   // link: "https://efreeinvoice.com",
   //   thumbnail:
-  //     "shared/DSC_2454.jpg",
+  //     "shared/DSC_2454.webp",
   // },
   // {
   //   title: "",
   //   // link: "https://goldenbellsacademy.com",
   //   thumbnail:
-  //     "shared/DSC_2089.jpg",
+  //     "shared/DSC_2089.webp",
   // },
   // {
   //   title: "",
   //   // link: "https://invoker.lol",
   //   thumbnail:
-  //     "shared/DSC_2453.jpg",
+  //     "shared/DSC_2453.webp",
   // },
   
 ];
