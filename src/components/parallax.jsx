2aa167555491863fb5c0df7c0a9da3cfb3da88b7
Nanda Lal Das <tikludas01@gmail.com>
2024-12-11 12:08:16 +0530

    removed extra images from parallaax

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -10,96 +10,96 @@ export function Parallax() {
 export const products = [
   {
     title: "",
-    link: "https://gomoonbeam.com",
+    // link: "https://gomoonbeam.com",
     thumbnail:
       "shared/DSC_0080.JPG",
   },
   {
     title: "",
-    link: "https://cursor.so",
+    // link: "https://cursor.so",
     thumbnail:
       "shared/DSC06699.JPG",
   },
   {
     title: "",
-    link: "https://userogue.com",
+    // link: "https://userogue.com",
     thumbnail:
       "shared/DSC06711.JPG",
   },
 
   {
     title: "",
-    link: "https://editorially.org",
+    // link: "https://editorially.org",
     thumbnail:
       "shared/DSC06734.JPG",
   },
   {
     title: "",
-    link: "https://editrix.ai",
+    // link: "https://editrix.ai",
     thumbnail:
       "shared/DSC06844.JPG",
   },
   {
     title: "",
-    link: "https://app.pixelperfect.quest",
+    // link: "https://app.pixelperfect.quest",
     thumbnail:
       "shared/DSC_0115.JPG",
   },
 
   {
     title: "",
-    link: "https://algochurn.com",
+    // link: "https://algochurn.com",
     thumbnail:
       "shared/DSC_0140.JPG",
   },
   {
     title: "",
-    link: "https://ui.aceternity.com",
+    // link: "https://ui.aceternity.com",
     thumbnail:
       "shared/_MG_0130.jpg",
   },
   {
     title: "",
-    link: "https://tailwindmasterkit.com",
+    // link: "https://tailwindmasterkit.com",
     thumbnail:
       "shared/DSC_9364.JPG",
   },
   {
     title: "",
-    link: "https://smartbridgetech.com",
+    // link: "https://smartbridgetech.com",
     thumbnail:
       "shared/DSC_2439.jpg",
   },
-  {
-    title: "",
-    link: "https://renderwork.studio",
-    thumbnail:
-      "shared/DSC06708.JPG",
-  },
+  // {
+  //   title: "",
+  //   // link: "https://renderwork.studio",
+  //   thumbnail:
+  //     "shared/DSC06708.JPG",
+  // },
 
-  {
-    title: "",
-    link: "https://cremedigital.com",
-    thumbnail:
-      "shared/DSC_2502.JPG",
-  },
-  {
-    title: "",
-    link: "https://efreeinvoice.com",
-    thumbnail:
-      "shared/DSC_2454.jpg",
-  },
-  {
-    title: "",
-    link: "https://goldenbellsacademy.com",
-    thumbnail:
-      "shared/DSC_2089.jpg",
-  },
-  {
-    title: "",
-    link: "https://invoker.lol",
-    thumbnail:
-      "shared/DSC_2453.jpg",
-  },
+  // {
+  //   title: "",
+  //   // link: "https://cremedigital.com",
+  //   thumbnail:
+  //     "shared/DSC_2502.JPG",
+  // },
+  // {
+  //   title: "",
+  //   // link: "https://efreeinvoice.com",
+  //   thumbnail:
+  //     "shared/DSC_2454.jpg",
+  // },
+  // {
+  //   title: "",
+  //   // link: "https://goldenbellsacademy.com",
+  //   thumbnail:
+  //     "shared/DSC_2089.jpg",
+  // },
+  // {
+  //   title: "",
+  //   // link: "https://invoker.lol",
+  //   thumbnail:
+  //     "shared/DSC_2453.jpg",
+  // },
   
 ];
