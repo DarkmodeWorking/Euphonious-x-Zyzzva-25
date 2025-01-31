5e75fe49f79297e18a470e0e6c17c831d1e4aeea
Nanda Lal Das <tikludas01@gmail.com>
2025-01-31 20:36:09 +0530

    went back to images instead of URLs cuz it didn't improve performance

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -12,63 +12,63 @@ export const products = [
     title: "",
     // link: "https://gomoonbeam.com",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334417/zyzzva/fte8caeyijc3ji4aoy69.jpg",
+      "shared/DSC_0080.webp",
   },
   {
     title: "",
     // link: "https://cursor.so",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334412/zyzzva/mhkrfpppqg68zxilwynz.webp",
+      "shared/DSC06699.webp",
   },
   {
     title: "",
     // link: "https://userogue.com",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334415/zyzzva/thkfqchmg1hzixil8hp3.jpg",
+      "shared/DSC06711.webp",
   },
 
   {
     title: "",
     // link: "https://editorially.org",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334414/zyzzva/bulj3ghqkdn4yn6lzpem.webp",
+      "shared/DSC06734.webp",
   },
   {
     title: "",
     // link: "https://editrix.ai",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334412/zyzzva/gxogbz2p7ainpl5ptbug.webp",
+      "shared/DSC06844.webp",
   },
   {
     title: "",
     // link: "https://app.pixelperfect.quest",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334413/zyzzva/obas3d1mp1vsspyrzij7.webp",
+      "shared/DSC_0115.webp",
   },
 
   {
     title: "",
     // link: "https://algochurn.com",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334412/zyzzva/cgx09kvo7glazbzbueqy.webp",
+      "shared/DSC_0140.webp",
   },
   {
     title: "",
     // link: "https://ui.aceternity.com",
     thumbnail:
-      "https://res.cloudinary.com/dwt633pc6/image/upload/v1738334412/zyzzva/gxogbz2p7ainpl5ptbug.webp",
+      "shared/_MG_0130.webp",
   },
   {
     title: "",
     // link: "https://tailwindmasterkit.com",
-    thumbnail:"https://res.cloudinary.com/dwt633pc6/image/upload/v1738334412/zyzzva/y2nksf3jqcvew35rsik4.webp"
-      
+    thumbnail:
+      "shared/DSC_9364.webp",
   },
   {
     title: "",
     // link: "https://smartbridgetech.com",
     thumbnail:
-      "",
+      "shared/DSC_2439.webp",
   },
   // {
   //   title: "",
